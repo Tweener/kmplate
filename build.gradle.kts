ac89613e6af21f4cf57ac2314e3629be7f459cb7
Vivien Mahé <vivouneuh@gmail.com>
2023-07-16 13:32:52 +0200

    Updates dependency updates checker to only stable versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version(Dependencies.Versions.gradle).apply(false)
@@ -10,3 +12,21 @@ plugins {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+
+// region Configure dependencyUpdates task
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        candidate.version.isNonStable()
+    }
+}
+
+fun String.isNonStable(): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(this)
+    return isStable.not()
+}
+
+// endregion Configure dependencyUpdates task
