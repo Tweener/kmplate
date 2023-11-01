03cac88e0adbb631f9664c782896ced1b1a0e987
Vivien Mahé <vivouneuh@gmail.com>
2023-11-01 19:08:54 +0100

    Updates MyApplication class to call NapierConfiguration

--- androidApp/src/main/java/com/tweener/changehere/android/MyApplication.kt
+++ androidApp/src/main/java/com/tweener/changehere/android/MyApplication.kt
@@ -2,16 +2,20 @@ package com.tweener.changehere.android
 
 import android.app.Application
 import com.tweener.changehere.android._internal.di.module.rootModule
-import io.github.aakira.napier.DebugAntilog
-import io.github.aakira.napier.Napier
+import com.tweener.changehere.android._internal.libs.napier.NapierConfiguration
+import com.tweener.changehere.android._internal.os.thread.CoroutinesThreadDispatcher
 import org.kodein.di.DI
 import org.kodein.di.DIAware
+import org.kodein.di.instance
 
 /**
  * @author Vivien Mahe
  * @since 05/06/2023
  */
 class MyApplication : Application(), DIAware {
+
+    private val threadsDispatcher: CoroutinesThreadDispatcher by instance()
+    private val napierConfiguration: NapierConfiguration by instance()
 
     override val di by DI.lazy {
         import(rootModule(context = this@MyApplication))
@@ -20,6 +24,6 @@ class MyApplication : Application(), DIAware {
     override fun onCreate() {
         super.onCreate()
 
-        Napier.base(DebugAntilog())
+        napierConfiguration.init()
     }
 }
