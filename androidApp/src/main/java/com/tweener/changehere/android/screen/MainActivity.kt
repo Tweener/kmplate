fb27152ba29996e692ce631df6889adbe7d796d3
Vivien Mahé <vivouneuh@gmail.com>
2024-02-14 14:41:09 +0100

    Remove DI MainModule from MainActivity

--- androidApp/src/main/java/com/tweener/changehere/android/screen/MainActivity.kt
+++ androidApp/src/main/java/com/tweener/changehere/android/screen/MainActivity.kt
@@ -4,15 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.tweener.changehere.presentation.MainScreen
-import com.tweener.changehere.android.screen._di.mainModule
-import org.kodein.di.Copy
-import org.kodein.di.DIAware
-import org.kodein.di.android.closestDI
-import org.kodein.di.android.retainedSubDI
 
-class MainActivity : ComponentActivity(), DIAware {
-
-    override val di by retainedSubDI(closestDI(), copy = Copy.All) { import(mainModule) }
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
