19c7b629c27b73b9dc02b4a17ddc0adb8d5301d6
Vivien Mahé <vivouneuh@gmail.com>
2024-05-13 12:49:49 +0200

    Adds a RealmLocalDateMapper to map LocalDate <> RealmInstant

--- shared/data/src/commonMain/kotlin/com/tweener/changehere/data/_internal/di/MapperModule.kt
+++ shared/data/src/commonMain/kotlin/com/tweener/changehere/data/_internal/di/MapperModule.kt
@@ -1,6 +1,8 @@
 package com.tweener.changehere.data._internal.di
 
 import com.tweener.changehere.data.source.firebase.remoteconfig.mapper.RemoteConfigFeatureFlagModelMapper
+import com.tweener.changehere.data.source.realm.mapper.RealmLocalDateMapper
+import com.tweener.changehere.data.source.realm.mapper.RealmLocalDateTimeMapper
 import org.koin.dsl.module
 
 /**
@@ -12,5 +14,9 @@ val mapperModule = module {
 
     // Remote Config
     factory { RemoteConfigFeatureFlagModelMapper() }
+
+    // Realm
+    factory { RealmLocalDateMapper() }
+    factory { RealmLocalDateTimeMapper() }
 
 }
